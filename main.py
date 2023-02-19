d07c9dac15edcd7925439021f683b3ad73c25646
CPK <CPKbaidu@163.com>
2023-02-19 14:17:54 +0800

    update get thumbnails online

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+import io
 import json
+import httpx
+import threading
 import webbrowser
 from tkinter import *
 from tkinter import messagebox
@@ -10,10 +13,20 @@ WINDOW_TITLE = "Nitrome Flash Games Launcher: Candytown"
 START_BUTTON_TEXT = "▶"
 INIT_TEXT = "Click a game tag to start!"
 ABOUT_TEXT = """Programmed by CPK\nSpecial thanks to Nitrome Bar"""
+THUMBNAIL_LOADING_TEXT = "Loading thumbnail..."
+THUMBNAIL_FAIL_TEXT = "Connection failed"
 GITHUB_URL = r'https://github.com/CPKaq/nitrome-game-launcher'
+THUMBNAIL_PREFIX = r'https://cdn.nitrome.com/images/thumbnails/'
 
 # Global variable for game icon
 _img_png = None
+
+
+def thrd(func, *args):
+    t = threading.Thread(target=func, args=args)
+    t.setDaemon(True)
+    t.start()
+    # t.join()
 
 
 class GameInfo:
@@ -58,6 +71,23 @@ def start_game(game: GameInfo):
     os.system('start /B flash {}'.format(game.get_param('url')))
 
 
+def get_thumbnail(url: str, label_icon: Label):
+    global _img_png
+    _img_png = None
+    label_icon.config(image=_img_png)
+    label_icon.config(text=THUMBNAIL_LOADING_TEXT)
+    try:
+        img_open = httpx.get(url, verify=False).content
+        data_stream = io.BytesIO(img_open)
+        pil_image = Image.open(data_stream)
+        _img_png = ImageTk.PhotoImage(pil_image)
+    except Exception:
+        label_icon.config(image=_img_png)
+        label_icon.config(text=THUMBNAIL_FAIL_TEXT)
+    else:
+        label_icon.config(image=_img_png)
+
+
 # Select game
 def set_game(clicked_game, game_list, selected_game, label_title, label_icon):
     for game in game_list.list():
@@ -65,10 +95,13 @@ def set_game(clicked_game, game_list, selected_game, label_title, label_icon):
             selected_game.set_game(game)
             break
     label_title.config(text=selected_game.get_param('name'))
-    img_open = Image.open('img/ico/{}'.format(selected_game.get_param('img')))
-    global _img_png
-    _img_png = ImageTk.PhotoImage(img_open)
-    label_icon.config(image=_img_png)
+    # img_open = Image.open('img/ico/{}'.format(selected_game.get_param('img')))
+    # global _img_png
+    # _img_png = ImageTk.PhotoImage(img_open)
+    # img_open = httpx.get(THUMBNAIL_PREFIX + selected_game.get_param('img'), verify=False).content
+    # get_thumbnail(THUMBNAIL_PREFIX + selected_game.get_param('img'))
+    thrd(get_thumbnail, THUMBNAIL_PREFIX + selected_game.get_param('img'), label_icon)
+    # label_icon.config(image=_img_png)
 
 
 def main():
